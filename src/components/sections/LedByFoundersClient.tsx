30c5e8609de702d23d4741f81deb0fd2f45e455d
Vishesh17 <visheshgpt17@gmail.com>
2026-08-13 17:04:02 +0530

    read full bio

--- src/components/sections/LedByFoundersClient.tsx
+++ src/components/sections/LedByFoundersClient.tsx
@@ -290,7 +290,7 @@ function SocialLink({
             style={LABEL_STYLE}
           >
             <span className="relative">
-              Read full story
+              Read Full Bio
               <span className="absolute -bottom-1 left-0 h-[1px] w-full origin-left scale-x-0 bg-black transition-transform duration-300 ease-out group-hover/link:scale-x-100" />
             </span>
             <span className="transition-transform duration-300 ease-[cubic-bezier(0.22,1,0.36,1)] group-hover/link:translate-x-1">
